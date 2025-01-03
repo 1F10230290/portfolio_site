bd1f2d7ecbfb292b2babfa6ef4316ba126e7daaf
1F102302900 <s1f102302900@iniad.org>
2025-01-03 18:11:26 +0900

    制作物の画面

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('Works_game',views.Works_game, name="Works_game"),
+    path('Harusion', views.Harusion, name='Harusion'),
+    path('GBW', views.GBW, name='GBW'),
+    path('Collider', views.Collider, name='Collider'),
+    path('E_Racer', views.E_Racer, name='E_Racer'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,15 @@ def home(request):
 
 def Works_game(request):
     return render(request, 'Works_game.html')
+
+def Harusion(request):
+    return render(request, 'Harusion.html')
+
+def GBW(request):
+    return render(request, 'Get-Back-Winter.html')
+
+def Collider(request):
+    return render(request, 'Collider.html')
+
+def E_Racer(request):
+    return render(request, 'E_Racer.html')
